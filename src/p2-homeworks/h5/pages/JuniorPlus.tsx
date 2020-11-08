bd6fd266604724cabe74683db342b4bf2903b0ce
Анатолий Митрошин <mymailtolaaa@gmail.com>
2020-11-08 20:11:05 +0300

    some changes

--- src/p2-homeworks/h5/pages/JuniorPlus.tsx
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import HW1 from "../../h1/HW1";
+import HW13 from "../../hw13/HW13";
 
 function JuniorPlus() {
     return (
         <div>
             <HW1/>
-
-            {/*<HW6/>*/}
+            <HW13/>
         </div>
     );
 }
